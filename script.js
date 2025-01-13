a7a4a570e60d156f9018438e815215fbaedab22d
Syl'Kyla Cole <sylkylacole@gmail.com>
2025-01-13 14:09:50 -0600

    I WENT TO THE WRONG PAGE LOL

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const submitButton = document.getElementById("submitButton");
 
 submitButton.addEventListener("click", function() {
   window.alert("Submitted!");
-  window.location.href = "index.html";
+  window.location.href = "complete.html";
 });
