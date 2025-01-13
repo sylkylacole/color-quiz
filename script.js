e22210ae7864c668b710f9b066855e07a7121494
Syl'Kyla Cole <sylkylacole@gmail.com>
2025-01-13 13:26:35 -0600

    Attempt #3 (I think I lost count)

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const submitButton = document.getElementById("submitButton");
+// const submitButton = document.getElementById("submitButton");
 
-submitButton.addEventListener("click", function() {
-  window.alert("Submitted!") && window.location.href = "completed.html";
-});
+// submitButton.addEventListener("click", function() {
+//   window.alert("Submitted!");
+//   window.location.href = "completed.html";
+// });
 
-submitButton.addEventListener("click", function() {
+// submitButton.addEventListener("click", function() {
+//   window.location.href = "completed.html";
+// });
+
+function popup() {
+  alert("Submitted!");
+}
+
+function redirect() {
   window.location.href = "completed.html";
-});
+}
